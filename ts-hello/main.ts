3e4e0165872e66701cf7db9509abffe339c62476
jasonthorne <jasonthorne@live.ie>
2019-02-27 20:40:29 +0000

    Objects 10

--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,5 +1,5 @@
 
-//+++++++++++++++++VARIABLE TYPES. Typescript 5.
+//+++++++++++++++++Typescript 5. VARIABLE TYPES. 
 
 //'let' is important here as it gives proper scope to the variable. ('var' gives global scope instead):
 let count = 5; //has to now be a number. Cant be changed on fly like js can!
@@ -26,7 +26,7 @@ let backgroundColour = colour.Red;
 
 
 
-//+++++++++++++++++TYPE ASSERTIONS. Typescript 6. 
+//+++++++++++++++++ Typescript 6. TYPE ASSERTIONS.
 
 let message = 'abc'; //initially declared as string
 let endsWithc = message.endsWith('c'); //acess granted to string methods as initially declared as string
@@ -38,7 +38,7 @@ let alsoEndsWithf = (message2 as string).endsWith('f'); //another way of doing i
 
 console.log("Boolean results: " + endsWithc, endsWithf, alsoEndsWithf);
 
-//+++++++++++++++++ARROW FUNCTIONS (lambda). Typescript 7. 
+//+++++++++++++++++Typescript 7. ARROW FUNCTIONS (lambda). 
 
 //regular function:
 let regularFunction = function(myMessage){
@@ -51,7 +51,7 @@ let arrowFunction = (myMessage) => console.log(myMessage); //can exclude braces 
 regularFunction("Yo");
 arrowFunction("dawg!");
 
-//+++++++++++++++++INTERFACES. Typescript 8. 
+//+++++++++++++++++Typescript 8. INTERFACES.  
 
 ///create drawPoint method:
 let drawPoint = (myPointObj) => {
@@ -82,17 +82,17 @@ let drawPoint3 = (myPointObj: Point) => {
     //...
 }  
 
-//+++++++++++++++++CLASSES. Typescript 9. 
+//+++++++++++++++++Typescript 9. CLASSES.
 
 //Classes group variables (properties) and functions (methods) that are highly related
 
 class ExampleClass {
-    x: number; //instance variable
-    y: number;  //instance variable
+    x: number; //instance var
+    y: number;  //instance var
 
     //method (a function within a class)
     exampleMethod(){
-        //...
+       console.log("X: " + this.x + " Y: " + this.y); //'this' is needed here for reference. (cant just use 'x'!)
     }
 
     //method
@@ -100,6 +100,19 @@ class ExampleClass {
 
     }
 }
+
+//+++++++++++++++++Typescript 10. OBJECTS.  
+
+//declare a var of class type:
+let exampleObj2 = new ExampleClass(); //No annotation needed as will be infered. Unnecessary annotation example:- let exampleObj: ExampleClass = new ExampleClass();
+exampleObj2.x = 1;
+exampleObj2.y = 2;
+exampleObj2.exampleMethod(); //call object's method 
+
+//+++++++++++++++++Typescript 11. CONSTRUCTORS. 
+
+
+
 
 
 
