6949a2413e1efd192d656f44b2a8b0035abe3fb1
jasonthorne <jasonthorne@live.ie>
2019-11-05 15:36:49 +0000

    1.1 - 1

---
+++ Workspace/1.1_ReviewingOCAConcepts/src/cat/Cat.java
@@ -0,0 +1,17 @@
+package cat;
+
+public abstract class Cat { //public class HAS to have the same name as the file name
+	
+	abstract void clean();
+	
+}
+
+
+class Lion extends Cat{
+
+	@Override
+	void clean() {
+		
+	}
+	
+}
