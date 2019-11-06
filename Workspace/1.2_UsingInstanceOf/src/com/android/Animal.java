21f3d0f9737c026f8b033102b41153542dac7e24
jasonthorne <jasonthorne@live.ie>
2019-11-06 12:23:33 +0000

    1.2 - 2

--- Workspace/1.2_UsingInstanceOf/src/com/android/Animal.java
+++ Workspace/1.2_UsingInstanceOf/src/com/android/Animal.java
@@ -1,5 +1,62 @@
 package com.android;
 
 public class Animal {
-
+	
+ /*
+  * checks to see if the animal sent to this method is only of one of the designated subclasses of Animal.
+  * If it is a designated animal, it then calls the appropriate method for that object.
+  */
+	
+	//this method an ONLY take an Animal or subclass of Animal
+	static void moveAnimal(Animal myAnimal) { //when an x object passed in here the object is an Animal reference to an x object (denying access to its own methods).
+		
+		/*
+		 * if this animal is a Dog, this is an Animal reference to a Dog object. So this object has no access to the bark() method. 
+		 * So we need to cast this object back to being a Dog reference to a Dog object.
+		 */
+		
+		if(myAnimal instanceof Dog)
+			((Dog)myAnimal).bark(); //cast Animal obj to be a Dog, and call it's bark method
+		else if(myAnimal instanceof Cat)
+			((Cat)myAnimal).purr();
+		else if(myAnimal instanceof Fish)
+			((Fish)myAnimal).swim();
+		else if(myAnimal instanceof Bird)
+			((Bird)myAnimal).fly();
+		else 
+			throw new RuntimeException("unsupported Animal");
+	}
+	
 }
+
+
+//subclasses of Animal: 
+
+class Dog extends Animal{
+	void bark() {
+		System.out.println("Dog barking");
+	}
+}
+
+class Cat extends Animal{
+	void purr() {
+		System.out.println("Cat purring");
+	}
+}
+
+class Fish extends Animal{
+	void swim() {
+		System.out.println("Fish swimming");
+	}
+}
+
+class Bird extends Animal{
+	void fly() {
+		System.out.println("Bird flying");
+	}
+}
+
+class Lizard extends Animal{
+	
+}
+
--- Workspace/1.2_UsingInstanceOf/src/com/android/Main.java
+++ Workspace/1.2_UsingInstanceOf/src/com/android/Main.java
@@ -9,7 +9,18 @@ public class Main {
 		/*
 		 * InstanceOf returns a boolean, but will ONLY COMPILE for something that COULD be true.
 		 * There has to be some sort of relationship inorder for the code to compile.
+		 * 
+		 * Instanceof ONLY WORKS with objects. Doesn't work for primitives. eg:
+		 * 
+		 *System.out.println(1 instanceof Integer); //WILL NOT COMPILE 
+		 *
+		 *
+		 *this WILL compile as the number 5 is autoboxed to become an Integer, which is an object and can be used with instanceof:
 		 */
+		Integer number = 5;
+		System.out.println(number instanceof Integer);
+		
+		//--------------------------------------------------------------------------------
 		
 		HeavyAnimal heavyHippo = new Hippo(); //this is a heavyAnimal superclass reference to a subclass Hippo object.
 		
@@ -33,6 +44,60 @@ public class Main {
 		
 		//henry is a Hippo reference to a Hippo object, so this will not compile as henry could NEVER be an Elephant:
 		//System.out.println(henry instanceof Elephant); ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		
+	
+		//CAN check literal object, such as string "hello":
+		System.out.println("hello" instanceof String); //true
+		System.out.println(new Animal() instanceof Animal); //true
+		
+		
+		//-----------------------------------------------------------------------------------
+		
+		/*
+		 * All objects are instances of the Object class, as every object inherits from the Object class. 
+		 * There is 1 case where some variable of object type instanceOf returns false, and that is where a variable is set to null.
+		 */
+		
+		System.out.println("instanceof Object  class");
+		
+		System.out.println(henry instanceof Object); //true
+		Animal nullAnimal = null; 
+		
+		//If you use instanceof with a null object, this is the only circumstance that will return false when checking to instanceof Object.
+
+		System.out.println(nullAnimal instanceof Object); //false
+		//Every other object will return true for instanceof Object:
+		System.out.println(heavyHippo instanceof Object); //true
+		System.out.println("nice day" instanceof Object); //true
+		
+		
+		/*
+		 * You cant have an object of an interface. IE new Behaviour(),
+		 * However you can use instanceof with an interface.
+		 * You can check to see if ANY class YOU CREATE implements ANY interface YOU CREATE.
+		 */
+		
+		System.out.println(henry instanceof Behaviour); //false
+		System.out.println(new Cow() instanceof Behaviour); //false
+		System.out.println(new PygmyHippo() instanceof Behaviour); //true
+		
+		
+		/*you CANT compare references of prebuilt objects (like String) against your own classes/interfaces 
+		 * As the answer will always be false.
+		 */
+		//System.out.println("hello" instanceof Behaviour); //will not compile
+		
+		
+		//======================================================================================
+		
+		//call moveAnimal method and pass it animals:
+		
+		System.out.println("");
+		Animal.moveAnimal(new Dog());
+		Animal.moveAnimal(new Cat());
+		Animal.moveAnimal(new Fish());
+		Animal.moveAnimal(new Bird());
+		//Animal.moveAnimal(new Lizard()); //this will throw a runtime exception (but will compile)
 
 	}
 
