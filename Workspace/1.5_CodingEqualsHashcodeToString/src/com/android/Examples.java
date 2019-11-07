bfcb8e01f5e0280c9a1221bec5576d548f0c0b95
jasonthorne <jasonthorne@live.ie>
2019-11-07 11:41:06 +0000

    1.5 - 2

--- Workspace/1.5_CodingEqualsHashcodeToString/src/com/android/Examples.java
+++ Workspace/1.5_CodingEqualsHashcodeToString/src/com/android/Examples.java
@@ -17,6 +17,10 @@ public class Examples {
 		
 		System.out.println(names.toString()); //println implicitly calls the String toString method
 		
+		
+		
+		
+		
 		/*
 		 * toString() method is a method of the string class and is called automatically whenever an object of any type is placed within a print statement. 
 		 * You can also explicitly call the toString()
@@ -33,13 +37,118 @@ public class Examples {
 		
 		System.out.println(myCards);
 		
-		
 		Animal andy = new Animal();
 		Animal angela = new Animal(3, "angela");
 		
 		System.out.println(andy);
 		System.out.println(angela);
 		
+		List<Animal>animals=Arrays.asList(andy, angela, new Animal(4, "Anthony"));
+		System.out.println("animals: " + animals);
+		
+		List<Cards>cards=Arrays.asList(new Cards(), new Cards(), new Cards());
+		System.out.println("cards: " + cards);
+		
 	}
+	
+	
+	static void ex2() {
+		
+		System.out.println("overriding equals method");
+		
+		
+		/*
+		 * public boolean equals(Object obj) is a method of the Object class and again, every object that is created in java will inherit this method. 
+		 * This method is used to check if two objects are the same.
+		 * If two objects are the same this method will return true. False if not the same.
+		 * If a class does NOT override this method, it will take its implementation from the Object class. 
+		 * This returns true if they are the same object, and false in all other circumstances. 
+		 * 
+		 * If a class overrides the equals() method, it is up to the programmer to determine what will constitute if two objects are the same, and return true. 
+		 * So equals can be used to only check certain variables of an object if that is what the program requires. 
+		 * Equals is usually overridden in conjunction with hashcode. Hashcode can be used in the equals method. 
+		 */
+		
+		/*
+		 * == is for checking if 2 PRIMITIVES have the same values, or if 2 references reference to the same object.
+		 * 
+		 */
+		
+		//primitives:
+		int num = 5;
+		int num2 = 5;
+		int num3 = 10;
+		
+		System.out.println(num==num2); //true
+		System.out.println(num==num3); //false
+		
+		
+		//objects:
+		Animal andy = new Animal();
+		Animal angela = andy;
+		Animal anthony = new Animal();
+		
+		System.out.println(andy==angela); //true - checks if the references point to the same object
+		System.out.println(angela==anthony); //false 
+		
+		//++++++++++++++++++++++++++++++++++++++++++++++IMPORTANT
+		//String has it's own overridden equals() method, which returns true if the CONTENTS of the string are the same. 
+		//It doesnt matter if they are different strings. 
+		
+		String s1 = new String("lion"); //- not in the string pool
+		String s2 = new String("lion"); //- not in the string pool
+		String s3 = "lion"; //in string pool
+		
+		System.out.println(s1==s2); //false
+		System.out.println(s1.equals(s2)); //true as equals CHECKS THE CONTENTS
+		System.out.println(s1.equals(s3)); 
+		
+		
+		System.out.println("checking cards");
+		//at this point, the cards class has no overridden equals() method, so it uses the equals() method from the object class.
+		//which just checks to see if they are the same object. 
+		
+		Cards myCard = new Cards();
+		Cards myCard2 = new Cards();
+		Cards myCard3 = myCard;
+		System.out.println(myCard == myCard2); //false
+		System.out.println(myCard == myCard2); //false
+		
+		System.out.println("Checking StringBuilders");
+		
+		/*
+		 * Stringbuilder doesn not have its own equals method, so it uses the Object class'es equals method.
+		 * Whixh only checks if they are the same object. 
+		 */
+		
+		StringBuilder sb1 = new StringBuilder("lion");
+		StringBuilder sb2 = new StringBuilder("lion");
+		System.out.println(sb1.equals(sb2));
+		
+		System.out.println("using toString to find if two stringbuilders are the same");
+		
+		System.out.println(sb1.toString().equals(sb2.toString()));
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
