ce7ccebc2be421c79aed29556191e16603873abd
jasonthorne <jasonthorne@live.ie>
2019-11-19 15:13:41 +0000

    2.4 - 2

--- Workspace/2.4_UnderstandingDesignPrinciples/src/com/android/Examples.java
+++ Workspace/2.4_UnderstandingDesignPrinciples/src/com/android/Examples.java
@@ -1,5 +1,62 @@
 package com.android;
 
 public class Examples {
+	
+	static void ex1() {
+		
+		
+		try {
+			//valid args will print out what we want
+			Human harry = new Human("Harry", 5);
+			System.out.println("Our human is created.");
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		
+		
+		try {
+			//invalid string
+			Human helen = new Human(null, 24);
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		
+		try {
+			//invalid string
+			Human harriet = new Human(" ", 24);
+			System.out.println("human with spaces created");
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		
+		try {
+			//invalid string
+			Human bob = new Human("", 24);
+			System.out.println("human with no length string created");
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		
+		try {
+			//invalid age
+			Human bill = new Human("ben", -1);
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		
+		try {
+			//invalid sting and age. only gets to invalid sting message 
+			Human bob = new Human("", -1);
+		}catch(Exception e) {
+			System.out.println(e.getMessage()); //second error message isnt shown
+		}
+		
+		
+	}
 
 }
--- Workspace/2.4_UnderstandingDesignPrinciples/src/com/android/Main.java
+++ Workspace/2.4_UnderstandingDesignPrinciples/src/com/android/Main.java
@@ -1,9 +1,31 @@
 package com.android;
 
+/**
+ * 
+ * A DESIGN PRINCIPLE
+ * 
+ * An established idea or best practices that facilitate the software design process.
+ * We deal with 5 main ones:
+ * 
+ * + INVARIANTS -
+ * A property of truth that is maintained even after the data is being modified. ie every human that is born has an age greater than 0. An
+ * 
+ * 
+ * + IS-A RELATIONSHIP
+ * + HAS-A RELATIONSHIP
+ * + OBJECT COMPOSITION
+ * + JAVABEAN
+ *
+ *
+ */
+
+
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		Examples.ex1();
 
 	}
 
