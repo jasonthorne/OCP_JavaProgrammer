a2db277ed003a8a628a33dc0b0d2b974d37e51a6
jasonthorne <jasonthorne@live.ie>
2019-11-22 12:09:34 +0000

    2.5C - 1

--- Workspace/2.5C_WorkingWithDesignPatterns/src/com/android/Examples.java
+++ Workspace/2.5C_WorkingWithDesignPatterns/src/com/android/Examples.java
@@ -1,5 +1,27 @@
 package com.android;
 
+import com.android.Flower.FlowerBuilder;
+
 public class Examples {
+	
+	static void ex1() {
+		Flower myFlower = new FlowerBuilder().setAge(12).build();
+		System.out.println(myFlower);
+		
+		Flower myFlower2 = new FlowerBuilder().setAge(30).setHeight(33).build();
+		System.out.println(myFlower2);
+		
+		//------------
+		
+		System.out.println("Flower with just an age:");
+		Flower myFlower3 = new FlowerBuilder().setAge(12).build();
+		System.out.println(myFlower3);
+	}
+	
+	
+	static void ex2() {
+		Animal myAnimal=new AnimalBuilder().setAge(12).setName("Cow").build();
+		System.out.println("\n" + myAnimal);
+	}
 
 }
--- Workspace/2.5C_WorkingWithDesignPatterns/src/com/android/Main.java
+++ Workspace/2.5C_WorkingWithDesignPatterns/src/com/android/Main.java
@@ -6,17 +6,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Flower myFlower = new FlowerBuilder().setAge(12).build();
-		System.out.println(myFlower);
-		
-		Flower myFlower2 = new FlowerBuilder().setAge(30).setHeight(33).build();
-		System.out.println(myFlower2);
-		
-		//------------
-		
-		System.out.println("Flower with just an age:");
-		Flower myFlower3 = new FlowerBuilder().setAge(12).build();
-		System.out.println(myFlower3);
+		Examples.ex1();
+	
+		Examples.ex2();
 		
 	}
 
