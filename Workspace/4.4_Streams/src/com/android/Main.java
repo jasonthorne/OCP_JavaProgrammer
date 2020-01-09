482b2db07492242a581c0ee44f84a5b3dc8b262d
jasonthorne <jasonthorne@live.ie>
2020-01-09 12:22:54 +0000

    4.4 - 1

---
+++ Workspace/4.4_Streams/src/com/android/Main.java
@@ -0,0 +1,23 @@
+package com.android;
+
+public class Main {
+	
+	/**
+	 * 
+	 *STREAMS
+	 *
+	 *These are NOT file input or output streams. Which are something completely different and are covered in chapter 8.
+	 *
+	 *A stream is NOT a data structure. I.e not an arrayList etc. 
+	 *Instead it takes input from COLLECTIONS, ARRAYS or I/O CHANNELS, or we can generate data using iterate and generate.
+	 *
+	 *Streams dont usually change the original data structure (but they can).
+	 * If its based on a list it will usually not change the original data, but it can.
+	 */
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
