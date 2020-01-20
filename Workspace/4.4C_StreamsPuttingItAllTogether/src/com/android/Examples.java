5ec7df85cbc0fceabb34cd18481291e32cdc0b0b
jasonthorne <jasonthorne@live.ie>
2020-01-20 12:06:24 +0000

    4.4c - 2

--- Workspace/4.4C_StreamsPuttingItAllTogether/src/com/android/Examples.java
+++ Workspace/4.4C_StreamsPuttingItAllTogether/src/com/android/Examples.java
@@ -1,7 +1,10 @@
 package com.android;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Examples {
 	
@@ -18,12 +21,100 @@ public class Examples {
 		
 		System.out.println("using lists and loops:");
 		
-		List<String>names=Arrays.asList("bob", "stewart", "jenkins", "cuthbert", "purple-monkey-dishwasher");
+		List<String>names=Arrays.asList("bob", "stewart", "jenkins", "huberty", "purple-monkey-dishwasher");
+		
+		//we need a new list that's going to hold our final liat of strings:
+		List<String>filtered=new ArrayList<String>();
+		
+		//we use this loop to populate our filtered arrayList with names 7 characters long.
+		for(String name: names) {
+			if(name.length()==7) {
+				filtered.add(name);
+			}
+		}
+		
+		//filtered now contains: "stewart", "jenkins", "huberty", but isn't sorted. So we use 
+		
+		Collections.sort(filtered);
+		System.out.println("filtered is now: " + filtered);
+		
+		if(filtered.size()>2) {
+			System.out.println("size of filtered list is: " + filtered.size());
+			
+			int size=filtered.size()-1;
+			
+			/*
+			 * start at size of list index and work down until it gets to index 1.
+			 */
+			for(int i=size; i>1; i--) {
+				filtered.remove(i);
+			}
+		}
+		
+		System.out.println("our final list is:" + filtered);
+		
+		
+		
+		
+		//---------------------FILTERED VERSION OF ABOVE:
+		
+		names.stream()
+		.filter(s -> s.length()==7)
+		.sorted()
+		.limit(2)
+		.forEach(System.out::println);
+		
+		//save above to a new array:
+		List<String>list = names.stream()
+				.filter(s -> s.length()==7)
+				.sorted()
+				.limit(2)
+				.collect(Collectors.toList());
+		
+		
+		System.out.println("our final list is:" + list);
+		
+		
+		//============================ORDER OF STREAM OPERATIONS+++++++++++++++++++++++++
+		
+		
+		/*
+		 * The order of stream operations DOES MATTER as can be shown in this example:
+		 */
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
 		
 		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
