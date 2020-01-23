b2f15d7e0b17c6fe3396b680dbd9da8941013621
jasonthorne <jasonthorne@live.ie>
2020-01-23 12:20:33 +0000

    4.6 - 2

--- Workspace/4.6_WorkingWithAdvancedStreamPipelineConcepts/src/com/android/Examples.java
+++ Workspace/4.6_WorkingWithAdvancedStreamPipelineConcepts/src/com/android/Examples.java
@@ -1,6 +1,9 @@
 package com.android;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class Examples {
 	
@@ -64,9 +67,22 @@ public class Examples {
 			System.out.println("s is a: " + s.getClass().getSimpleName());
 		});
 		
+		//-------------------------------
+		
+		List<Integer>nums=Stream.generate(()->(int)(Math.random()*100))
+				.distinct()
+				.limit(5)
+				.peek(System.out::println)
+				.collect(Collectors.toList());
 		
 		
+		Optional<Integer>optMax = nums.stream().max((n1,n2)->n1-n2);
 		
+		System.out.println(optMax); //prints out the optional
+		
+		System.out.println(optMax.orElse(0)); //prints out the actual int or 0
+		
+		System.out.println(optMax.orElseGet(()->(int)(Math.random()*100))); //prints out the actual int ora random int.
 		
 		
 		
