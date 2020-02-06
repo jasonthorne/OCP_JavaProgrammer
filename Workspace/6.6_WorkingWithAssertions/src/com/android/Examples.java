5f72b25405d63d64a76b5249c3c91244b209d8a2
jasonthorne <jasonthorne@live.ie>
2020-02-06 15:12:47 +0000

    6.5 - 3

---
+++ Workspace/6.6_WorkingWithAssertions/src/com/android/Examples.java
@@ -0,0 +1,32 @@
+package com.android;
+
+
+
+public class Examples {
+	
+	static void ex1() { 
+		
+		System.out.println("\nex1:");
+		
+		//======================================CONTROL FLOW INVARIANT:
+		
+		/*
+		  Below does nothing if assertions are turned off. 
+		 It produces an exception if assertionsd are turned on.
+		 */
+		//assert false;
+		
+		Seasons mySeason = Seasons.SPRING;
+		
+		
+		
+		
+		
+		
+	}
+
+}
+
+enum Seasons{
+	SPRING,SUMMER,AUTUMN,WINTER;
+}
---
+++ Workspace/6.6_WorkingWithAssertions/src/com/android/Main.java
@@ -0,0 +1,17 @@
+package com.android;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Examples.ex1();
+		
+		/*
+		 * Below does nothing if assertions are turned off. 
+		 * It produces an exception if assertionsd are turned on.
+		 */
+		//assert false;
+
+	}
+
+}
