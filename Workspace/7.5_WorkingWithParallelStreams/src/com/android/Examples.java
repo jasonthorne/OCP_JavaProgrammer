c542ebddf4b84cc80042b540d8d93800383f64c7
jasonthorne <jasonthorne@live.ie>
2020-03-03 12:00:29 +0000

    7.4 - 11

--- Workspace/7.5_WorkingWithParallelStreams/src/com/android/Examples.java
+++ Workspace/7.5_WorkingWithParallelStreams/src/com/android/Examples.java
@@ -16,27 +16,76 @@ public class Examples {
 		
 		System.out.println(list); //print list
 		
-		
+		//EVERYTHING in this stream is done one after another:
 		list.stream().forEach((n)->System.out.print(n+ " ")); //print in the ORDER they are in the list, using stream
 		
 		System.out.println();
+		System.out.println("amount of threads: " + Thread.activeCount()); //amount of threads: 1 (uses just the system thread). 
 		
-		
-		//prints the numbers in the list HOWEVER there is NO ORDER, so this could be a lot faster than using a regular stream.
-		list.parallelStream().forEach((n)->System.out.print(n + " ")); 
+		//prints the numbers in the list HOWEVER there is NO ORDER, as x threads can access this at a single time.
+		list.parallelStream().forEach((n)->System.out.print(n + " "));  //this is a multi-threaded stream.
 		
 		System.out.println();
 		
 		//this changes the stream back to an ordered stream:
-		list.parallelStream() //this allows more than one thread to act upon this stream at the same time
-		.filter((n)->n>50) //this is still a parallell stream
-		.forEachOrdered((n)->System.out.print(n + " ")); //this is now a single threaded stream
+		list.parallelStream() //this allows more than one thread to act upon this stream at the same time using the max amount of threads on your system.
+		.filter((n)->n>50) //this is still a parallel stream
+		.forEachOrdered((n)->System.out.print(n + " ")); //this is now a SINGLE threaded stream (not multi-threaded)
 		
+		System.out.println();
+		System.out.println("amount of threads: " + Thread.activeCount()); //amount of threads: 4
 		
 		//===========================
 		
+	}
+	
+	
+	static void ex2() {
 		
+		System.out.println("\nex2");
+	
+		//Sorted Streams================================
+		
+		
+		//15 random numbers:
+		List<Integer>list = Stream.generate(()->(int)(Math.random()*100)+1).limit(15).collect(Collectors.toList());
+		
+		//sort the random numbers of the list:
+		list.stream().sorted().forEach((i)->System.out.print(i + ","));
+		
+		
+		System.out.println();
+		
+		/*
+		 * This is an example of certain stream operations (when used with parallel streams) not working the same as with ordinary single threaded streams.
+		 */
+		list.parallelStream().sorted().forEach((i)->System.out.print(i + ","));
+		
+		System.out.println();
+		
+		
+		list.parallelStream().sorted().forEachOrdered((i)->System.out.print(i + ","));
 		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
